bdb508315fad4914e304b325c3dea50084ff248e
krzysiek.jekaterynczuk <k.jekaterynczuk@gmail.com>
2017-06-25 17:55:31 +0200

    admin login site added

---
+++ admin/login.php
@@ -0,0 +1,30 @@
+<?php
+
+require_once '../src/Admin.php';
+require_once '../src/config.php';
+
+if($_SERVER['REQUEST_METHOD'] == 'POST') {
+    if(isset($_POST['email']) && strlen(trim($_POST['email'])) >=5
+        && isset($_POST['password']) && strlen(trim($_POST['password'])) >= 5) {
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+        
+        $admin = Admin::loadAdminByEmail($conn, $email);
+        if($admin) {
+            if(password_verify($password, $admin->getPassword())) {
+                $_SESSION['adminId'] = $admin->getId();
+                header('Location: panel.php');
+            }
+        } 
+    }
+    echo 'Wrong login or password.';
+} 
+
+?>
+
+<form method="POST" action="#">
+    <input name="email" type="email">
+    <input name="password" type="password">
+    <button type="submit">Sign in</button>
+</form>
+
