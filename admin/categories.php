d3cfdb245859fccac604875e21c42dbf2668655c
krzysiek.jekaterynczuk <k.jekaterynczuk@gmail.com>
2017-06-27 22:19:29 +0200

    adding category feature added

--- admin/categories.php
+++ admin/categories.php
@@ -1,5 +1,8 @@
 <?php
 
+require_once './../src/Category.php';
+require_once './../src/config.php';
+
 session_start();
 
 if(!isset($_SESSION['adminId'])) {
@@ -7,8 +10,31 @@ if(!isset($_SESSION['adminId'])) {
     exit();
 }
 
+if($_SERVER['REQUEST_METHOD'] == 'POST') {
+    if(isset($_POST['name']) && strlen(trim($_POST['name'])) >=3) {
+        $newCategory = new Category();
+        $newCategory->setName($_POST['name']);
+        
+        if($newCategory->saveToDB($conn)) {
+            echo 'success';
+        }
+    }
+}
 
 ?>
+<html>
+<!-- List of categories -->
+    <ul>
+        <?php
+            $categories = Category::loadAllCategories($conn);
+            for($i = 0; $i < count($categories); $i++) {
+                echo '<li>'.$categories[$i]->getName().'</li></br>';
+            }
+        ?>
+    </ul>
 
-<-- List of categories -->
-$categories
+    <form action="#" method="POST">
+        <input type="text" name="name">
+        <input type="submit" value="Add category">
+    </form>
+</html>
