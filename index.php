86485b3de127d247f19dbc0057c5466c79986e7d
Fylypek <fylypeksteam@gmail.com>
2017-07-06 15:22:56 +0200

    working on index

--- index.php
+++ index.php
@@ -1,6 +1,22 @@
-<?php
-
-echo("arekmarek");
-echo("siema");
-
-?>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" /> 
+    <head>
+        
+            <title>WebShop</title>
+    </head>
+    
+    <body>
+        <h1>Welcome to the WebShop adventurer!</h1>
+        
+        
+        <div>
+            <h2>
+                <b>Login</b>
+            </h2>
+            <form action="" method="POST">
+                <input name="login" type="text" placeholder="Login"/><br>
+                <input name="password" type="password" placeholder="Password"/><br>
+                <input type="submit" name="zaloguj" value="LOGIN"/>
+            </form>            
+        </div>
