2d15af7bef64817fcfa37fc761a81f2445f31f08
krzysiek.jekaterynczuk <k.jekaterynczuk@gmail.com>
2017-07-08 22:06:02 +0200

    style sheet added

--- index.php
+++ index.php
@@ -2,43 +2,11 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" /> 
     <head>
-            <title>WebShop</title>
-            <style>
-            body {margin:0;}
-
-            .topnav {
-              overflow: hidden;
-              background-color: #333;
-            }
-
-            .topnav a {
-              float: left;
-              display: block;
-              color: white;
-              text-align: center;
-              padding: 14px 16px;
-              text-decoration: none;
-              font-size: 17px;
-            }
-
-            .topnav a:hover {
-              background-color: #ddd;
-              color: black;
-            }
-
-            .topnav a.active {
-                background-color: blueviolet;
-                color: white;
-            }
-            .fatxt {
-                font-family: futura;
-                color: blanchedalmond;
-            }
-            </style>
-    
+        <title>WebShop</title>
+        <link rel="stylesheet" href="css/style.css">
     </head>
     
-    <body style="background-color: black;">
+    <body>
         
         <h1 class="fatxt">Welcome to the WebShop adventurer!</h1>
         
@@ -48,8 +16,7 @@
         <a href="#shop">Shop</a>
         <a href="#about">About us</a>
         </div>
-
-        
+  
         <div>
             <h2>
                 <b class="fatxt">Login</b>
@@ -60,3 +27,5 @@
                 <input type="submit" name="zaloguj" value="LOGIN"/>
             </form>            
         </div>
+    </body>
+</html>
