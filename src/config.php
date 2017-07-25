b3c877a06141786708ee29e550af4f4bb2f9295a
krzysiek.jekaterynczuk <k.jekaterynczuk@gmail.com>
2017-07-25 20:42:41 +0200

    new db connection

--- src/config.php
+++ src/config.php
@@ -1,11 +1,20 @@
 <?php
 
-require_once 'config_param.php';
-
-$conn = new mysqli($host, $db_user, $db_password, $db_name);
-
-if ($conn->connect_error) {
-    echo 'Connection error.' . $conn->connect_error . '<br/>';
-} else {
-    echo 'Connection works.<br/>';
+class Db extends PDO{
+    
+    public function __construct() {
+        $config = [
+            'db_type' => 'mysql',
+            'db_name' => 'workshop4',
+            'db_user' => 'root',
+            'db_pass' => 'coderslab',
+            'charset' => 'utf8',
+            'host' => 'localhost'
+        ];
+        parent::__construct("{$config['db_type']}:host={$config['host']};dbname={$config['db_name']}",
+            $config['db_user'], $config['db_pass'],
+            array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES {$config['charset']}"));
+    }
 }
+$dd = new Db();
+var_dump($dd);
